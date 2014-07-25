a32f06b1adc18f6833dd4f62b6eff2e6d9c755ac
chenzc <80132938@qq.com>
2014-07-25 18:11:05 +0800

    add read_file

---
+++ read_file.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl
+use Benchmark;
+
+my $LOG_FILE = 'wwplugin_resin_log.2014_07_21';
+
+my @EXT_LIST = map {qr/$_/} qw {
+	c.y.p.g.rds.service.RdsService
+};
+
+my $result = 0;
+my $startime = new Benchmark;
+
+open LOG_FILE, $LOG_FILE;
+my $exit_flag = 0;
+
+while (<LOG_FILE>) {
+	# if($exit_flag){
+		# last;
+	# }
+	
+	foreach my $ext (@EXT_LIST) 
+	{
+		#$result++ if $_ =~ /$ext/;
+		if($_ =~ /$ext/){
+			#print $_."\n";
+			$result++;
+			#$exit_flag=1;
+		}
+	}
+}
+
+close LOG_FILE;
+print "count result: $result \n";
+
+my $sum_count = $result * 4;
+print "sum count: $sum_count \n";
+
+printf "** %s\n\n", timestr(timediff(new Benchmark, $startime));
+print "end read file.\n";
