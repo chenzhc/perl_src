9e303ce69d1da85207459b0e4375e8a23e5281b5
chenzhc <80132938@qq.com>
2014-08-21 18:26:59 +0800

    add

--- regex_test.pl
+++ regex_test.pl
@@ -10,3 +10,20 @@ $_ = "Here is the text";
 print "@a\n";
 
 $s = "Here is the text";
+
+$wife{"Jacob"} = ("Leah","Rachel","Bilhah","Zilpah");
+
+print "$wife{Jacob}\n";
+
+$wife{"Jacob"} = ["Leah","Rachel","Bilhah","Zilpah"];
+
+print "$wife{Jacob}\n";
+
+$kide_of_wife{"Jacob"} = {
+	"Leah" => ["Reuben","Simeon","Levi","Judah","Issachar","Zebulun"],
+	"Rachel" => ["Joseph","Benjamin"],
+	"Bilhap" => ["Dan","Naphtali"],
+	"Zilpah" => ["Gad","Asher"]
+};
+
+print $kide_of_wife{"Jocob"}{"Leah"}[0];
