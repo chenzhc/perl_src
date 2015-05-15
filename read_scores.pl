6e623021949d9e8a65c5367606a0f013679b0a25
zhichao.chen <zhichao.chen@shuyun.com>
2015-05-15 18:00:18 +0800

    add

---
+++ read_scores.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+open(GRADES, "grades") or die "Can't open grades: $!\n";
+
+while($line = <GRADES>){
+	($student, $grade) = split(" ", $line);
+	$grades{$student} .= $grade ." ";
+}
+
+foreach $student (sort keys %grades){
+	$scores = 0;
+	$total = 0;
+	@grades = split(" ", $grades{$student});
+	foreach $grade(@grades){
+		$total += $grade;
+		$scores++;
+	}
+	$ave = $total / $scores;
+	print "$student: $grades{$student}\etAverage: $ave\n";
+}
